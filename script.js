c4d5d5575fb1f9a1151bce83001f44a20b3c2cd3
Everton Ito <ebatista.ito@gmail.com>
2023-05-29 21:33:00 -0300

    Exercícios aula resolvidos

---
+++ script.js
@@ -0,0 +1,56 @@
+// let numero = Number(prompt("Digite um número"))
+
+// while(condicao) {
+//     bloco de código
+//     incremento
+// }
+
+// let soma = 0
+// while(numero !== 0) {
+//     soma = soma + numero
+//     console.log(numero)
+//     console.log(soma)
+//     numero = Number(prompt("Digite um novo número"))
+// }
+
+// console.log("Soma", soma)
+
+// FOR
+
+// for(let i = 0; i <= numero; i++){
+//     console.log(i)
+// }
+
+// For com arrays
+
+// const array = [4, 89, 23, 10, 6, 65, 2]
+
+// for(let i=0; i < array.length -1; i++){
+//     console.log(`O elemento de índice ${i} é ${array[i]}`)
+// }
+
+// Exercício 01
+
+// let tipoUsuario = prompt("Qual seu tipo de usuário? ")
+
+// while(tipoUsuario.toUpperCase() !== `A`) {
+//     console.log("Acesso negado")
+//     tipoUsuario = (prompt("Qual o seu tipo de usuário?"))
+
+// }
+// console.log("Boas vindas, Admin")
+
+// Exercício 02
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(`2 x ${i} é igual a ${2*[i]}`)
+// }
+
+// Exercício 03
+
+const arrayNomes = ["neymar", "ganso", "durval", "arouca"]
+
+for(let i = 0; i < arrayNomes.length; i++) {
+    console.log(`O elemento de índice ${i} é o ${arrayNomes[i]}`)
+}
+
